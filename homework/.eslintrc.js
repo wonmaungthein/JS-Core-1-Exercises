5732f2ddfe068855a5f0e2ac484de18fd79aa22f
Won Maung Thein <wonmaungthein@gmail.com>
2017-06-06 16:18:33 +0100

    have done challenge 05

---
+++ homework/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console": 0,
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
