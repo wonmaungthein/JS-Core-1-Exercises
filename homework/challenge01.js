ff1788df6a951ab0028ed888e8fa81caaa071e5a
Won Maung Thein <wonmaungthein@gmail.com>
2018-03-27 14:53:31 +0100

    done challenge1 from homework

--- homework/challenge01.js
+++ homework/challenge01.js
@@ -39,3 +39,44 @@ var writers = [
 
 If you want an extra challenge, only `console.log()` the writers that are alive.
 */
+
+var writers = [
+  {
+    firstName: "Virginia",
+    lastName: "Woolf",
+    occupation: "writer",
+    age: 59,
+    alive: false
+  },
+  {
+    firstName: "Zadie",
+    lastName: "Smith",
+    occupation: "writer",
+    age: 41,
+    alive: true
+  },
+  {
+    firstName: "Jane",
+    lastName: "Austen",
+    occupation: "writer",
+    age: 41,
+    alive: false
+  },
+  {
+    firstName: "bell",
+    lastName: "hooks",
+    occupation: "writer",
+    age: 64,
+    alive: true
+  }
+];
+
+const loopWriters = writers.forEach((writer, i) => {
+  console.log(
+    `Hi, my name is ${writers[i].firstName} ${writers[i].lastName}. I am ${
+      writers[i].age
+    } years old, and work as a ${writers[i].occupation}.`
+  );
+});
+
+// DONE EXERCISE BUT HAVEN'T DONE ONLY ALIVE WRITER
